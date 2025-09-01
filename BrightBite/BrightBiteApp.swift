86dcfb7e87276fd1fe1c396615cc2c004573ba46
Tuan Nguyen <ben.nguyen08@icloud.com>
2025-09-01 16:04:27 -0400

    Initial Commit

---
+++ BrightBite/BrightBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightBiteApp.swift
+//  BrightBite
+//
+//  Created by Tuan Nguyen on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct BrightBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrightBiteTests/BrightBiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrightBiteTests.swift
+//  BrightBiteTests
+//
+//  Created by Tuan Nguyen on 9/1/25.
+//
+
+import Testing
+@testable import BrightBite
+
+struct BrightBiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
